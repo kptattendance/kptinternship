946a56c6f69d6487a833699273c258fc7291bb6b
kptattendance <kptattendance@gmail.com>
2026-09-13 13:57:17 +0530

    new year 2026

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,27 @@ import tailwindcss from "@tailwindcss/vite";
 import fs from "fs";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve";
 
-  server: {
-    host: "local.kptplacements.org",
-    port: 443,
-    strictPort: true,
+  return {
+    plugins: [react(), tailwindcss()],
 
-    https: {
-      key: fs.readFileSync("./local.kptplacements.org-key.pem"),
-      cert: fs.readFileSync("./local.kptplacements.org.pem"),
-    },
-  },
+    // HTTPS is required only for local development
+    server: isDev
+      ? {
+          host: "local.kptplacements.org",
+          port: 443,
+          strictPort: true,
+          https: {
+            key: fs.readFileSync(
+              "./local.kptplacements.org-key.pem"
+            ),
+            cert: fs.readFileSync(
+              "./local.kptplacements.org.pem"
+            ),
+          },
+        }
+      : undefined,
+  };
 });
